3589b41837fe384149bbebcff85a10cc3bf29dec
Roman Isopenko <isopenkoroman@gmail.com>
2023-06-09 22:51:21 +0200

    Add burger

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,14 @@ import Scroll from "./ScrollTrigger";
 import Carousel from "./Carousel";
 import onLoad from "./Preloader";
 
+//BURGER
+const burger = document.querySelector("[data-burger]");
+const menu = document.querySelector("[data-menu]");
+burger.addEventListener("click", function () {
+  this.classList.toggle("opened");
+  menu.classList.toggle("opened");
+});
+
 //ACTION BUTTON
 const action = document.querySelector("[data-action]");
 action.addEventListener("click", () => {
